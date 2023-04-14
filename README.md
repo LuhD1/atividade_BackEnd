91aeca71214b319bea9cd05ac4287d0c61d78358
LuhD1 <damasceno.lucas.o@gmail.com>
2023-04-14 19:52:01 -0300

    Ultimas tarefas finalizadas juntamente com todos os desafios

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # atividade_BackEnd
 Atividade Back-end SoulCode
+
+# Este é um código em JavaScript que utiliza o framework Express para criar um servidor web. Ele define uma rota GET e uma rota POST para manipular dados de alunos armazenados em um arquivo JSON.
+
+# O arquivo começa importando as dependências necessárias (Express, Colors, e FS). Em seguida, é definida uma função chamada novoJson() que escreve os dados de alunos em um arquivo chamado "db.json".
+
+# Depois, é criado um servidor Express usando app = express(). O servidor utiliza o middleware express.json() para analisar o corpo das solicitações em formato JSON.
+
+# A primeira rota GET é "/alunos", que retorna uma lista de alunos. Se forem fornecidos parâmetros de consulta, a rota filtra a lista com base nesses parâmetros e, em seguida, chama a função novoJson() para salvar as alterações em "db.json".
+
+# A segunda rota POST é "/alunos/novo", que cria um novo aluno. Se o corpo da solicitação não contiver informações suficientes, a rota retorna um código de status 400 (Bad Request). Caso contrário, a rota adiciona o novo aluno à lista de alunos e chama a função novoJson() para salvar as alterações em "db.json".
+
+# A terceira rota DELETE é "/alunos/:index", que remove um aluno com base em seu índice na lista de alunos. Se o índice não for válido, a rota retorna um código de status 400 (Bad Request). Caso contrário, a rota remove o aluno e chama a função novoJson() para salvar as alterações em "db.json".
+
+# A quarta rota POST é "/alunos/atualizar/:index", que atualiza as informações de um aluno com base em seu índice na lista de alunos. Se o índice não for válido ou o corpo da solicitação não contiver informações suficientes, a rota retorna um código de status 400 (Bad Request). Caso contrário, a rota atualiza as informações do aluno e chama a função novoJson() para salvar as alterações em "db.json".
+
+# Finalmente, o servidor começa a escutar na porta 3000 e exibe uma mensagem informando que o servidor foi iniciado com sucesso.
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const colors = require("colors");
-const alunos = require("./alunos");
+const alunos = require("./data/alunos");
+const fs = require("fs");
+const morgan = require("morgan");
+
+
+function novoJson() {
+  fs.writeFile("db.json", JSON.stringify(alunos), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Dados salvos em db.json");
+  });
+}
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/alunos", (req, res) => {
   const nome = req.query.nome;
@@ -19,6 +33,7 @@ app.get("/alunos", (req, res) => {
   if (media) {
     listaAlunos = listaAlunos.filter((elemento) => elemento.media === media);
   }
+  novoJson();
   res.json(listaAlunos);
 });
 
@@ -35,7 +50,54 @@ app.post("/alunos/novo", (req, res) => {
     media: media,
   };
   alunos.push(novoCadastro);
+  novoJson();
   res.status(201).json(novoCadastro);
+});
+
+app.delete("/alunos/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    res.status(400).send("Aluno não válido");
+    return;
+  }
+
+  const aluno = alunos[index];
+
+  if (!aluno) {
+    res.status(404).send("Aluno não encontrado");
+  } else {
+    alunos.splice(index, 1);
+    novoJson();
+    res.send("Aluno removido com sucesso");
+  }
+});
+
+app.put("/alunos/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    res.status(400).send("Aluno não válido");
+    return;
+  }
+
+  const { nome, media } = req.body;
+
+  if (!nome || !media) {
+    res.status(400).send("Dados inválidos");
+    return;
+  }
+
+  const aluno = alunos[index];
+
+  if (!aluno) {
+    res.status(404).send("Aluno não encontrado");
+  } else {
+    aluno.nome = nome;
+    aluno.media = media;
+    novoJson();
+    res.send("Aluno atualizado com sucesso");
+  }
 });
 
 app.listen(3000, () => {
